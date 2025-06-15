8643f53371cc058eac68abda23bd07c35ce483aa
pedrocorreia-ieg <pedro.correia@germinare.org.br>
2025-06-15 16:32:18 -0300

    Iniciando projeto

---
+++ src/main/java/com/example/Simulado/SimuladoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simulado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladoApplication.class, args);
+	}
+
+}
