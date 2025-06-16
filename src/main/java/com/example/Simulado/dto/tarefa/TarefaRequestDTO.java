13912a8fe5c18d1d48a2d3d8181433915f879466
rkhue <85773727+rkhue@users.noreply.github.com>
2025-06-15 23:12:19 -0300

    (feat) Mensagens explicativas de validação nos DTOs

--- src/main/java/com/example/Simulado/dto/tarefa/TarefaRequestDTO.java
+++ src/main/java/com/example/Simulado/dto/tarefa/TarefaRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.Simulado.dto.tarefa;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,8 +15,12 @@ import org.springframework.validation.annotation.Validated;
 @NoArgsConstructor
 @Builder
 public class TarefaRequestDTO {
-    @Size(max = 150)
+
+    @NotNull(message = "A tarefa precisa de um título")
+    @NotEmpty(message = "O título não pode ser vazio")
+    @Size(max = 150, message = "O título deve ter no máximo 150 caracteres")
     private String titulo;
 
+    @NotEmpty(message = "A descrição não pode ser vazia")
     private String descricao;
 }
--- src/main/java/com/example/Simulado/dto/usuario/UsuarioRequestDTO.java
+++ src/main/java/com/example/Simulado/dto/usuario/UsuarioRequestDTO.java
@@ -17,13 +17,14 @@ import org.springframework.validation.annotation.Validated;
 @Builder
 public class UsuarioRequestDTO {
 
-    @NotNull
-    @NotEmpty
-    @Size(max = 100)
+    @NotNull(message = "O nome é obrigatório")
+    @NotEmpty(message = "O nome não pode ser vazio")
+    @Size(max = 100, min = 2, message = "O nome deve ter entre 2 e 100 caracteres")
     private String nome;
 
-    @NotEmpty
-    @Email
-    @Size(max = 150)
+    @NotNull(message = "O email é obrigatório")
+    @NotEmpty(message = "O email não pode ser vazio")
+    @Email(message = "O email deve ser válido")
+    @Size(max = 150, message = "O email deve ter no máximo 150 caracteres")
     private String email;
 }
